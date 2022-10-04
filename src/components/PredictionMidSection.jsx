4ab92e8386d030f24591d7842da624e9ab895923
Maxwell Mawube <59958815+maxxies@users.noreply.github.com>
2022-10-04 16:25:44 +0000

    Handle news submition

--- src/components/PredictionMidSection.jsx
+++ src/components/PredictionMidSection.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { useState } from "react";
 import Button from "./Button";
 
 function PredictionMidsection(props) {
+    const [newsBody, setNewsBody] = useState("");
+    const [social, setSocial] = useState(0);
+    const [environmental, setEnvironmental] = useState(0);
+    const [governance, setGovernance] = useState(0);
+
+    function handleNewsSubmit() {
+        if (newsBody === "") {
+            return;
+        } else {
+            const data = {
+                data: newsBody,
+            };
+
+            try {
+                fetch("", {
+                    method: "POST",
+                    headers: {
+                        accept: "application.json",
+                        "Content-Type": "application/json",
+                    },
+                    body: data,
+                })
+                    .then((response) => {
+                        if (response.ok) {
+                            return response.json();
+                        } else {
+                            alert("Something went wrong. try again");
+                        }
+                    })
+                    .then((data) => {
+                        if (data.status === true) {
+                            setSocial(data.results["social"]);
+                            setGovernance(data.results["governance"]);
+                            setEnvironmental(data.results["environmental"]);
+                        } else if (data.status === false) {
+                            alert("Analysis on data failed, try again");
+                        }
+                    });
+            } catch (error) {
+                alert(error);
+            }
+        }
+    }
     return (
         <>
             <div className={props.styles.midSection}>
@@ -13,6 +57,9 @@ function PredictionMidsection(props) {
                                 className={props.styles.textareaBox}
                                 id="textarea"
                                 placeholder="Write your news here..."
+                                onChnge={(event) =>
+                                    setNewsBody(event.target.value)
+                                }
                             ></textarea>
                         </div>
                     </div>
@@ -20,7 +67,42 @@ function PredictionMidsection(props) {
                         <Button name={"Submit"} />
                     </div>
                 </div>
-                <div className={props.styles.rightPanel}>hi</div>
+                <div className={props.styles.rightPanel}>
+                    <div className={props.styles.resultTitle}>
+                        {" "}
+                        <h3>Results</h3>
+                    </div>
+                    <div className={props.styles.results}>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <p>Environmental: </p>
+                                    </td>
+                                    <td>
+                                        <span>{`${environmental}%`} </span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <p>Social: </p>
+                                    </td>
+                                    <td>
+                                        <span>{`${social}%`} </span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <p>Governance: </p>
+                                    </td>
+                                    <td>
+                                        <span>{`${governance}%`} </span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </>
     );
